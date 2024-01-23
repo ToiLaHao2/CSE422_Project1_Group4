a434061812171c4ab2b87a375c65a0f99a853299
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-01-23 15:59:36 +0700

    rename package

--- ChatApplication/src/test/java/group4/ChatApplication/useCases/UserRegistrationTest.java
+++ ChatApplication/src/test/java/group4/ChatApplication/useCases/UserRegistrationTest.java
@@ -1,10 +1,11 @@
-package group4.ChatApplication.useCases;
+package group4.chatApplication.useCases;
+
 import java.security.Identity;
 import java.util.Optional;
 
 import group4.chat.usecases.adapters.DataStorage;
 import group4.chat.usecases.users.UserRegistration;
-import group4.chat.usecases.users.UserRegistration.*;
+
 import group4.chat.domains.User;
 import group4.chat.infrastructure.data.InMemoryDataStorage;
 import group4.chat.infrastructure.services.MD5Hasher;
@@ -13,21 +14,19 @@ import group4.chat.usecases.users.UserRegistration.ResultCodes;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
-   // @org.junit.jupiter.api.BeforeEach
+    // @org.junit.jupiter.api.BeforeEach
     public void setUp() {
         DataStorage storage = InMemoryDataStorage.getInstance();
         storage.getUsers().add(new User("phuc", "1234"));
     }
 
-    //@org.junit.jupiter.api.AfterEach
+    // @org.junit.jupiter.api.AfterEach
     public void tearDown() {
         DataStorage storage = InMemoryDataStorage.getInstance();
         storage.cleanAll();
     }
 
-    
-
-    //@Test
+    // @Test
     public void addUserSuccessfully() throws Exception {
 
         UserRegistration.InputValues input = new UserRegistration.InputValues("phuc", "1234");
@@ -36,10 +35,6 @@ public class UserRegistrationTest {
         UserRegistration registration = new UserRegistration(storage, new MD5Hasher());
         UserRegistration.OutputValues output = registration.execute(input);
 
-        
     }
 
-
-
-   
 }
