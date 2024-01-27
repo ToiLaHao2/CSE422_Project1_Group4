622d0f256772ed3db81d74eb7b01040be1377cbc
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-01-27 08:15:19 +0700

    update p1-2

--- ChatApplication/src/main/java/group4/chat/domains/groupUser/publicGroup/PublicGroup.java
+++ ChatApplication/src/main/java/group4/chat/domains/groupUser/publicGroup/PublicGroup.java
@@ -1,13 +1,11 @@
 package group4.chat.domains.groupUser.publicGroup;
 
-import group4.chat.domains.User;
 import group4.chat.domains.groupUser.GroupUsers;
 
 public class PublicGroup extends GroupUsers {
 
-    private User _admin;
-
     public PublicGroup() {
         super();
     }
+
 }
