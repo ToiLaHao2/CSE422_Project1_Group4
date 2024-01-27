4e133ae7ed3f51de81470f70b1ced36e7aa75f6e
maidayne <mai.nguyenthi.cit19@eiu.edu.vn>
2024-01-27 08:30:24 +0700

    fix testcase command and import exercise 1

--- ChatApplication/src/test/java/group4/chapApplication/useCases/UserRegistrationTest.java
+++ ChatApplication/src/test/java/group4/chapApplication/useCases/UserRegistrationTest.java
@@ -1,7 +1,11 @@
-package group4.chatApplication.useCases;
+package group4.chapApplication.useCases;
 
-import java.security.Identity;
+
 import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import group4.chat.usecases.adapters.DataStorage;
 import group4.chat.usecases.users.UserRegistration;
@@ -11,22 +15,21 @@ import group4.chat.infrastructure.data.InMemoryDataStorage;
 import group4.chat.infrastructure.services.MD5Hasher;
 import group4.chat.usecases.users.UserRegistration.OutputValues;
 import group4.chat.usecases.users.UserRegistration.ResultCodes;
-import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
-    // @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     public void setUp() {
         DataStorage storage = InMemoryDataStorage.getInstance();
         storage.getUsers().add(new User("phuc", "1234"));
     }
 
-    // @org.junit.jupiter.api.AfterEach
+    @AfterEach
     public void tearDown() {
         DataStorage storage = InMemoryDataStorage.getInstance();
         storage.cleanAll();
     }
 
-    // @Test
+    @Test
     public void addUserSuccessfully() throws Exception {
 
         UserRegistration.InputValues input = new UserRegistration.InputValues("phuc", "1234");
