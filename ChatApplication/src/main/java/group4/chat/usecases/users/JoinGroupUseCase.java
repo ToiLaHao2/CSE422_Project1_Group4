c3ea12aefa3d6b846ff0a590d0131da2dad07e36
tranphuquycubin <131520832+tranphuquycubin@users.noreply.github.com>
2024-01-30 15:50:23 +0700

    update from Thursday

--- ChatApplication/src/main/java/group4/chat/usecases/users/JoinGroupUseCase.java
+++ ChatApplication/src/main/java/group4/chat/usecases/users/JoinGroupUseCase.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 import group4.chat.domains.User;
 import group4.chat.domains.groupUser.publicGroup.PublicGroup;
+import group4.chat.usecases.UseCase;
+import group4.chat.usecases.adapters.DataStorage;
 
-public class JoinGroupUseCase {
+public class JoinGroupUseCase
+ extends UseCase<JoinGroupUseCase.InputValues, JoinGroupUseCase.OutputValues>  {
     private String joinCode;
     private List<User> members=new ArrayList<>();
     private String groupName;
     private PublicGroup publicGroup;
     private User user;
+    private DataStorage _dataStorage;
+
     public JoinGroupUseCase(String joinCode) {
         this.joinCode = joinCode;
     }
@@ -24,16 +29,49 @@ public class JoinGroupUseCase {
     public String getGroupName() {
         return groupName;
     }
-    public boolean joinGroupByJoinCode(String code){
-        if (code.equals(joinCode)) {
-            members.add(user);
-            System.out.println("Join group successfully");
-            return true;
-        } else {
-            System.out.println("Invalid join code. Failed to join group.");
-            return false;
+    public static class InputValues {
+        private String _groupID;
+        private String _userID;
+
+        public InputValues(String groupID, String userID) {
+            _groupID = groupID;
+            _userID  = userID;
+        }
+    
+    }
+    public static class OutputValues {
+        private final int _resultCode;
+        private final String _message;
+
+        public OutputValues(int resultCode, String message) {
+            _message = message;
+            _resultCode = resultCode;
+        }
+
+        public int getResultCode(){
+            return _resultCode;
+        }
+
+        public String getMessage(){
+            return _message;
         }
     }
-    
-    
+
+    public static class ResultCodes {
+        public static final int SUCCESS = 1;
+        public static final int FAILED = 0;
+    }
+
+    @Override
+    public OutputValues execute(InputValues input) throws Exception {
+        _dataStorage.getUsers().getById(_userID);
+        if (input.equals(joinCode)) {
+            members.add();
+            System.out.println("Joined group '" + groupName + "' successfully!");
+            return true;
+    }
 }
+    
+    
+    
+
